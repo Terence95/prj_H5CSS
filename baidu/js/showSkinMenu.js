efbd523aa3cce2b8078ff7f4bbfb8444d7ea6c9d
Terence <wrze@sina.com>
2016-10-06 02:11:25 +0800

    modify

--- baidu/js/showSkinMenu.js
+++ baidu/js/showSkinMenu.js
@@ -1,12 +1,33 @@
 // 这个是导航栏登录设置的hover显示模块
 define(['jquery'], function($) {
+    var isDown = false;
+
     $('.change-skin-btn').click(function(event) {
-      /* Act on the event */
-      $('.skin-container').slideDown(400);
+        /* Act on the event */
+        if (!isDown) {
+            $('.skin-container').slideDown(400);
+            isDown = true;
+        }
     });
     $('.roll-up').click(function(event) {
-      /* Act on the event */
-      $('.skin-container').slideUp(400);
+        /* Act on the event */
+        if (isDown) {
+            $('.skin-container').slideUp(400);
+            isDown = false;
+        }
+
     });
-    
+
+    $('#main-wrap').click(function(event) {
+        /* Act on the event */
+        if (isDown) {
+            $('.skin-container').slideUp(400);
+            isDown = false;
+        }
+
+    });
+
+
+
+
 });
